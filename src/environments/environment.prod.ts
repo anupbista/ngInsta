c50475f87ffb4a5baa89f5224a73525c51fc0a59
anupbista <bistaanup77@gmail.com>
2020-07-31 20:38:43 +0545

    Environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  // production: true,
-  // api: "https://nginsta.herokuapp.com/",
-  // postImageApi: "https://nginsta.herokuapp.com/posts/image/",
-  // profileImageApi: "https://nginsta.herokuapp.com/user/image/",
   production: true,
-  api: "http://192.168.0.25:3002/",
-  postImageApi: "http://192.168.0.25:3002/posts/image/",
-  profileImageApi: "http://192.168.0.25:3002/user/image/",
+  api: "https://nginsta.herokuapp.com/",
+  postImageApi: "https://nginsta.herokuapp.com/posts/image/",
+  profileImageApi: "https://nginsta.herokuapp.com/user/image/",
+  // production: true,
+  // api: "http://192.168.0.25:3002/",
+  // postImageApi: "http://192.168.0.25:3002/posts/image/",
+  // profileImageApi: "http://192.168.0.25:3002/user/image/",
 };
